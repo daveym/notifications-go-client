93fea0e8978b8111c5c19f8736777d8e4b9abcb5
daveym <davey.mcglade@gmail.com>
2016-04-30 15:25:57 +0100

    model work

--- api/model/SmsRequest.go
+++ api/model/SmsRequest.go
@@ -1,26 +1,50 @@
 package model
 
+import "github.com/pkg/errors"
+
 // SmsRequest - Represents an SMS request
 type SmsRequest interface {
-	getPhoneNumber() string
-	getTemplateId() string
-	getPersonalisation() personalisation
+	GetEmail() string
+	GetTemplateId() string
+	GetPersonalisation() personalisation
 }
 
 type smsRequest struct {
-	_phonenumber     string
+	_phoneNumber     string
 	_templateID      string
 	_personalisation personalisation
 }
 
-func (p *smsRequest) getPhoneNumber() string {
-	return p._phonenumber
+func (p *smsRequest) GetPhoneNumber() string {
+	return p._phoneNumber
 }
 
-func (p *smsRequest) getTemplateID() string {
+func (p *smsRequest) GetTemplateID() string {
 	return p._templateID
 }
 
-func (p *smsRequest) getPersonalisation() personalisation {
+func (p *smsRequest) GetPersonalisation() personalisation {
 	return p._personalisation
+}
+
+func (p *smsRequest) Build(phoneNumber string, templateID string, pers personalisation) (smsRequest, error) {
+
+	var req smsRequest
+	var err error
+
+	if phoneNumber == "" {
+		errors.Wrap(err, "Phone number cannot be empty")
+		return req, err
+	}
+
+	if templateID == "" {
+		errors.Wrap(err, "Template id cannot be empty")
+		return req, err
+	}
+
+	req._phoneNumber = phoneNumber
+	req._templateID = templateID
+	req._personalisation = pers
+
+	return req, err
 }
