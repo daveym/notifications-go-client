8433132acc28ddc8231130d592553a728fbe172b
daveym <davey.mcglade@gmail.com>
2016-04-30 20:20:44 +0100

    Model work

--- api/model/Personalisation.go
+++ api/model/Personalisation.go
@@ -1,4 +1,32 @@
 package model
 
+import "github.com/pkg/errors"
+
+// Personalisation - Represents information used to personalize a message sent through GovNotify service.
+type Personalisation interface {
+	asMap() string
+}
+
 type personalisation struct {
+	_store map[string]string
+}
+
+func (p *personalisation) asMap() map[string]string {
+	return p._store
+}
+
+func (p *personalisation) Build(name string, value string) (personalisation, error) {
+
+	var res personalisation
+	var err error
+
+	if name == "" || value == "" {
+		errors.Wrap(err, "Personalisation when set must have at least one field")
+		return res, err
+	}
+
+	p._store = make(map[string]string)
+	p._store[name] = value
+
+	return res, err
 }
