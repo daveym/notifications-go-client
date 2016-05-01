2ff2642968577d2280d10377ca36a289f2a7b11a
daveym <davey.mcglade@gmail.com>
2016-05-01 15:13:07 +0100

    Model work

---
+++ api/model/StatusResponse.go
@@ -0,0 +1,31 @@
+package model
+
+// StatusResponse - Represents status information of the already requested notifications.
+type StatusResponse interface {
+	GetID() string
+	GetStatus() string
+}
+
+type statusResponse struct {
+	_ID     string
+	_status string
+}
+
+func (p *statusResponse) GetID() string {
+	return p._ID
+}
+
+func (p *statusResponse) GetStatus() string {
+	return p._status
+}
+
+func (p *statusResponse) Build(id string, status string) (statusResponse, error) {
+
+	var res statusResponse
+	var err error
+
+	res._ID = id
+	res._status = status
+
+	return res, err
+}
