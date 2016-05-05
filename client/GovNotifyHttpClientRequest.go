cedd6abcb8a864c765bff9c79e6b8fd10caeb7a6
daveym <davey.mcglade@gmail.com>
2016-05-05 18:04:31 +0100

    Model work

--- client/GovNotifyHttpClientRequest.go
+++ client/GovNotifyHttpClientRequest.go
@@ -1,5 +1,18 @@
 package client
 
+// IGovNotifyHTTPClientRequest - Model for HTTP request to GovNotify
+type IGovNotifyHTTPClientRequest interface {
+	GetMethod() HTTPMethod
+	GetURL() string
+	GetBody() string
+	GetHeaders() map[string]string
+	Build()
+}
+
 // GovNotifyHTTPClientRequest - Model for HTTP request to GovNotify
-type GovNotifyHTTPClientRequest interface {
+type GovNotifyHTTPClientRequest struct {
+	_httpmethod HTTPMethod
+	_url        string
+	_body       string
+	_headers    map[string]string
 }
---
+++ client/HttpMethod.go
@@ -0,0 +1,7 @@
+package client
+
+// HTTPMethod - representation of HTTP methods used by the client.
+type HTTPMethod struct {
+	GET  string
+	POST string
+}
