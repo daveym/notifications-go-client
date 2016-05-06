72cc5e784b5048266ad365805bb55ff0086eb4d0
daveym <davey.mcglade@gmail.com>
2016-05-06 22:29:38 +0100

    Import issues

--- api/GovNotifyApi.go
+++ api/GovNotifyApi.go
@@ -2,5 +2,5 @@ package api
 
 // GovNotifyAPI - Client to handle communication with Gov Notify API.
 type GovNotifyAPI interface {
-	sendEmail(request EmailRequest) NotificationCreatedResponse
+	//sendEmail(request EmailRequest) NotificationCreatedResponse
 }
