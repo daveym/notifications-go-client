85d15405c2841321181dfa69ec420e42a322705c
daveym <davey.mcglade@gmail.com>
2016-05-07 09:16:52 +0100

    Initial client work

--- client/GovNotifyHttpClient.go
+++ client/GovNotifyHttpClient.go
@@ -1,6 +1,20 @@
 package client
 
-// GovNotifyAPI - Client to handle communication with Gov Notify API.
-type GovNotifyAPI interface {
-	send(request GovNotifyHTTPClientRequest) (GovNotifyHTTPClientResponse, error)
+// IGovNotifyAPI - Client to handle communication with Gov Notify API.
+type IGovNotifyAPI interface {
+	Send(request GovNotifyHTTPClientRequest) (GovNotifyHTTPClientResponse, error)
+}
+
+// GovNotifyAPI - Implementation for IGovNotifyAPI interface
+type GovNotifyAPI struct {
+}
+
+// Send -  Sends a request to GovNotify
+func (p *GovNotifyAPI) Send(request GovNotifyHTTPClientRequest) (GovNotifyHTTPClientResponse, error) {
+
+	var res GovNotifyHTTPClientResponse
+	var err error
+
+	return res, err
+
 }
