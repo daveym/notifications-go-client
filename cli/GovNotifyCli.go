77bc6e4ed6ac2593e24aea7e1fdfa43cba12e0e6
daveym <davey.mcglade@gmail.com>
2016-05-08 16:53:55 +0100

    Wrong cmd folder

--- cli/GovNotifyCli.go
+++ cli/GovNotifyCli.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/daveym/lint/cmd"
+import "github.com/daveym/notifications-go-client/cmd"
 
 // All commands are under the cmd directory
 // Command Logic is instigated from these files.
