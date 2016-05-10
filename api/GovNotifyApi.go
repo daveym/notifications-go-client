3f6cb981c2e02813c4d40ca8614205f15a391fcb
daveym <davey.mcglade@gmail.com>
2016-05-10 08:42:10 +0100

    Api work

--- api/GovNotifyApi.go
+++ api/GovNotifyApi.go
@@ -1,6 +1,23 @@
 package api
 
-// GovNotifyAPI - Client to handle communication with Gov Notify API.
-type GovNotifyAPI interface {
-	//sendEmail(request EmailRequest) NotificationCreatedResponse
+// IGovNotifyAPI - Client to handle communication with Gov Notify API.
+type IGovNotifyAPI interface {
+
+	// SendEmail - Emailrequest object representing request to notify by email
+	SendEmailReq(EmailRequest) NotificationCreatedResponse
+
+	// SendEmail -  Valid email address, TemplateID of a valid GovNotify Template
+	SendEmail(string, string)
+
+	// SendSms - SMSrequest object representing request to notify by email
+	SendSmsReq(SMSRequest) NotificationCreatedResponse
+
+	// SendSms -  Valid Phonenumber, TemplateID of a valid GovNotify Template
+	SendSms(string, string)
+
+	// checkStatus - Status request object representing request to check the status of notification
+	checkStatus(StatusRequest) StatusResponse
+
+	// checkStatus - notificationId id of the notification
+	checkStatus(string) StatusResponse
 }
