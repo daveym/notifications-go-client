99f347b752dec6db14ece8148ad3fba146323185
daveym <davey.mcglade@gmail.com>
2016-05-10 16:12:31 +0100

    Removing Viper/Cobra CLI imports

--- api/GovNotifyApi.go
+++ api/GovNotifyApi.go
@@ -4,20 +4,20 @@ package api
 type IGovNotifyAPI interface {
 
 	// SendEmail - Emailrequest object representing request to notify by email
-	SendEmailReq(EmailRequest) NotificationCreatedResponse
+	//SendEmailReq(EmailRequest) NotificationCreatedResponse
 
 	// SendEmail -  Valid email address, TemplateID of a valid GovNotify Template
-	SendEmail(string, string)
+	//SendEmail(string, string)
 
 	// SendSms - SMSrequest object representing request to notify by email
-	SendSmsReq(SMSRequest) NotificationCreatedResponse
+	//SendSmsReq(SMSRequest) NotificationCreatedResponse
 
 	// SendSms -  Valid Phonenumber, TemplateID of a valid GovNotify Template
-	SendSms(string, string)
+	//SendSms(string, string)
 
 	// checkStatus - Status request object representing request to check the status of notification
-	checkStatus(StatusRequest) StatusResponse
+	//checkStatus(StatusRequest) StatusResponse
 
 	// checkStatus - notificationId id of the notification
-	checkStatus(string) StatusResponse
+	//checkStatus(string) StatusResponse
 }
--- cli/GovNotifyCli.go
+++ cli/GovNotifyCli.go
@@ -1,9 +1,5 @@
 package main
 
-import "github.com/daveym/notifications-go-client/cmd"
-
-// All commands are under the cmd directory
-// Command Logic is instigated from these files.
 func main() {
-	cmd.Execute()
+
 }
